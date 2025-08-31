ea8ccd7ca37940036a04b8efb2af7eb33a766323
michael <michael.ramanampamonjy@ethumada.com>
2025-08-31 22:55:31 +0300

    feat: add exception

---
+++ src/main/java/com/chatbto/chatbot/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.chatbto.chatbot.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
